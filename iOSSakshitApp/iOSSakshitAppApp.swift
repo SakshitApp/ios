0b63f89ffeb5c99987bed3df6d9b1764a2d4bf1c
Punit Chhajer <punitchhajer@hotmail.com>
2021-03-19 23:25:48 +0530

    Initialize xcode project

---
+++ iOSSakshitApp/iOSSakshitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSSakshitAppApp.swift
+//  iOSSakshitApp
+//
+//  Created by Punit Chhajer on 19/03/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSSakshitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
